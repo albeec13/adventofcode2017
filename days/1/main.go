80ca81342fb8f4cd2c72bfa1f5714db42c3521ef
Albert Chaharbakhshi <albeec13@gmail.com>
2017-12-05 01:12:50 -0600

    Day 1, Puzzle 2

--- days/1/main.go
+++ days/1/main.go
@@ -7,26 +7,25 @@ import (
 
 func main() {
     captcha := 0
-    var prev byte
+    var half int
 
-    // If the input file has at least two values, put the last value into "prev" placeholder
-    // to handle the wrap around scenario first
-    if len(input) > 1 {
-        prev = input[len(input) - 1]
-    } else {
+    // If the input file does not have at least two values, or does not has an even number of values stop now 
+    // otherwise save the half-length of the input array
+    if !(len(input) > 1 && (len(input) % 2) == 0) {
         log.Fatal("Bad input file.")
+    } else {
+        half = len(input)/2
     }
 
-    // First pass will compare last value (above) with first value, and continue from there
+    // Otherwise, we'll compare each value to the value one-half the length of the ciricular array away
     for i := 0; i < len(input); i++ {
-        if prev == input[i] {
-            if prev > 47 && prev < 58 {
-                captcha += int(prev - 48)
+        if input[i] == input[(i + half) % len(input)] {
+            if input[i] > 47 && input[i] < 58 {
+                captcha += int(input[i] - 48)
             } else {
                 log.Fatal("Invalid number character in input.")
             }
         }
-        prev = input[i]
     }
     fmt.Println("Captcha: ", captcha)
 }
